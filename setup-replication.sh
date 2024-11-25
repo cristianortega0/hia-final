147a198b39a86699461295cb56777af41d1846e6
NikoVelazco <129793948+NikoVelazco@users.noreply.github.com>
2024-11-25 00:45:01 -0300

    por favor Dios

--- setup-replication.sh
+++ setup-replication.sh
@@ -22,7 +22,6 @@ CHANGE REPLICATION SOURCE TO
 mysql -u root -proot -e "START SLAVE;"
 
 
-
 #mysql -u root -proot -h mysql-maestro -P 3306 -e "CHANGE MASTER TO MASTER_HOST='mysql-maestro', MASTER_PORT=3306, MASTER_USER='root', MASTER_PASSWORD='root', MASTER_AUTO_POSITION=1;"
 #mysql -u root -proot -h mysql-esclavo -P 3306 -e "START REPLICA;"
 
